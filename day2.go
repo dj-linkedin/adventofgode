fe4687236e6115c0ab15d58fabaef550d8c05308
David Jagoda <djagoda@linkedin.com>
2019-12-25 15:12:34 -0800

    Add day2.go simple 1202 version

---
+++ day2.go
@@ -0,0 +1,68 @@
+// cat day2.input | go run day2.go
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func str_array_to_int_array(string_array []string) []int {
+	var i_a = []int{}
+	for _, s := range string_array {
+		i, _ := strconv.Atoi(s)
+		i_a = append(i_a, i)
+	}
+	return i_a
+}
+
+func int_array_to_str_array(int_array []int) []string {
+	var s_a = []string{}
+	for _, i := range int_array {
+		s := strconv.Itoa(i)
+		s_a = append(s_a, s)
+	}
+	return s_a
+}
+
+func process(i_a []int) []int {
+	cur_pos := 0
+	for {
+		cur_val := i_a[cur_pos]
+		if 99 == cur_val {
+			return i_a
+		}
+		if 1 == cur_val {
+			i_a[i_a[cur_pos+3]] =
+				i_a[i_a[cur_pos+1]] + i_a[i_a[cur_pos+2]]
+			cur_pos += 4
+			continue
+		}
+		if 2 == cur_val {
+			i_a[i_a[cur_pos+3]] =
+				i_a[i_a[cur_pos+1]] * i_a[i_a[cur_pos+2]]
+			cur_pos += 4
+			continue
+		}
+		fmt.Println(cur_val)
+		panic("error - invalid opcode")
+	}
+	return i_a
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		values := strings.Split(scanner.Text(), ",")
+		values[1] = "12"
+		values[2] = "2"
+		fmt.Println(strings.Join(
+			int_array_to_str_array(process(str_array_to_int_array(
+				values))), ","))
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
